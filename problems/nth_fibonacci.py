576896a7cc43e752f47f3b4577000b48c5740c37
Jack Perry <jack.e.perry@outlook.com>
2022-09-03 14:47:46 -0600

    👌 IMPROVE: finished fib problem

--- problems/nth_fibonacci.py
+++ problems/nth_fibonacci.py
@@ -1,6 +1,20 @@
 from typing import Dict
 
 
+# O(n) time | O(1) space
+def getNthFib(n):
+    last_two = [0, 1]
+    counter = 3
+    
+    while counter <= n:
+        next_fib = last_two[0] + last_two[1]
+        last_two[0] = last_two[1]
+        last_two[1] = next_fib
+        counter += 1
+
+    return last_two[1] if n > 1 else last_two[0]
+
+
 # O(n) time | O(n) space
 def getNthFib(n, memoize: Dict = {1: 0, 2: 1}) -> int:
     if n in memoize:
